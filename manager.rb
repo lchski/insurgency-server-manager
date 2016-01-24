c7fa691b8d16997401d14db4a21fa6f7c427aac3
Lucas Cherkewski <lucas@ecustom.ca>
2016-01-24 15:28:34 -0500

    add method to update duckdns records

--- manager.rb
+++ manager.rb
@@ -3,13 +3,12 @@ Dotenv.load
 
 require 'droplet_kit'
 require 'net/ssh'
+require 'curb'
 
 class InsurgencyManager
   def initialize(digital_ocean_client)
     @client = digital_ocean_client
     @snapshot_id = get_snapshot_id()
-
-
   end
 
   def get_snapshot_id()
@@ -19,6 +18,10 @@ class InsurgencyManager
         return image.id
       end
     end
+  end
+
+  def update_duckdns(new_ip)
+    Curl::Easy.perform("https://www.duckdns.org/update?domains=where-napoleon-died&token=#{ENV['DUCKDNS_TOKEN']}&ip=#{new_ip}")
   end
 end
 
