27a71e166ebf7a89784b6c2abd1616455cff885b
Lucas Cherkewski <lucas@ecustom.ca>
2016-01-24 15:25:17 -0500

    initial work on manager class

--- manager.rb
+++ manager.rb
@@ -1,2 +1,25 @@
 require 'dotenv'
 Dotenv.load
+
+require 'droplet_kit'
+require 'net/ssh'
+
+class InsurgencyManager
+  def initialize(digital_ocean_client)
+    @client = digital_ocean_client
+    @snapshot_id = get_snapshot_id()
+
+
+  end
+
+  def get_snapshot_id()
+    images = @client.images.all(public:false)
+    images.each do |image|
+      if image.name == "insurgency-server"
+        return image.id
+      end
+    end
+  end
+end
+
+im = InsurgencyManager.new(DropletKit::Client.new(access_token: ENV['DO_TOKEN']))
